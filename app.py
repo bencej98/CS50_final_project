7c2f42d65fcf89a84b26caa0ef8d334006dcb8b2
Bence Jenei <bencej9812@gmail.com>
2022-11-22 16:19:30 +0100

    Created index, layout, login, register.html and modifyed app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
+from helpers import login_required
+import sqlite3
 
 # Configure application
 app = Flask(__name__)
@@ -22,3 +24,64 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+'''
+@app.route('/register', methods=['GET', 'POST'])
+# Registers the user
+def register():
+    # When the site is opened via GET it gets displayed
+    if request.method == "GET":
+        return render_template("register.html")
+
+    #else:
+        
+'''
+
+
+
+
+# The login_required decorator requires the user to login before use
+#@login.required
+
+@app.route('/')
+#@login_required
+def main_page():
+    return render_template("index.html")
+
+
+'''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    #Forgets the user
+    session.clear()
+
+     # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Query database for username
+        # rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 400)
+        
+              # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+        
+'''
